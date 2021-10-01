08143557226f9509b8bb6b69becf2a7874c258eb
Mr.programmer2938 <78996423+mrprogrammer2938@users.noreply.github.com>
2021-10-01 13:10:26 -0700

    Add files via upload

---
+++ uninstall.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+# This code write by Mr.Programmer2938
+# Black-Tool 4.0.0
+# Uninstalling File
+if [[ "$(id -u)" -ne 0 ]]; then
+  echo "Please, Run This Programm as Root!"
+  exit 1
+fi
+function main() {
+        clear
+        echo "Uninstalling..."
+        sleep 2
+        cd .. && rm -r Black-Tool && cd C:\Program Files\b-pass\ && rm pass.txt && cd /usr/bin && rm hack && cd /usr/local/bin && rm hack
+        echo "
+  
+  Finish...!
+  "
+        echo "
+  Mr.nope: Good bye °^° 
+ 
+  "
+        exit 1
+}
+main
